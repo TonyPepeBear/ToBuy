f2cc5a135df7420fdd4fe95bb174a8033aa3743d
TonyPepe <tonybear55665566@gmail.com>
2020-02-18 03:02:29 +0800

    onBackPress

--- app/src/main/java/com/tonypepe/tobuy/MainActivity.kt
+++ app/src/main/java/com/tonypepe/tobuy/MainActivity.kt
@@ -50,4 +50,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer.closeDrawer(GravityCompat.START)
         return true
     }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else
+            super.onBackPressed()
+    }
 }
